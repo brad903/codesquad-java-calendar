74da9a5eca0641820e3bbda90fc729ab5b4a2974
Brad Lee <brad903@naver.com>
2018-08-03 12:56:39 +0900

    객체화
    객체화를 통해 복잡한 매개변수 정

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class BaseballGame {
 	// 여러 곳에서 사용되고 있는 자리수를 상수 값으로 구현(피드백 반영)
 	public static final int figures = 3;
+	int[] playerValue = new int[figures];
+	int[] pcValue = new int[figures];
 
 	// 컴퓨터 중복되지 않는 3자리 수 뽑기
 	public static int[] randomValue() {
@@ -38,8 +41,8 @@ public class BaseballGame {
 		return playerValue;
 	}
 
-	// 예외처리2 : 중복값 입력했을 경우(피드백 반영)
-	public static boolean duplicateException(int[] playerValue) {
+	// 예외처리 : 중복값 입력했을 경우(피드백 반영)
+	public boolean duplicateException(int[] playerValue) {
 		for (int i = 0; i < figures; i++) {
 			if (findDuplicate(i, playerValue)) {
 				return true;
@@ -48,8 +51,8 @@ public class BaseballGame {
 		return false;
 	}
 	
-	// 예외처리3 : 입력값에 0이 포함되었을 경우(피드백 반영)
-	public static boolean zeroException(int[] playerValue) {
+	// 예외처리 : 입력값에 0이 포함되었을 경우(피드백 반영)
+	public boolean zeroException(int[] playerValue) {
 		for(int i=0; i<figures; i++) {
 			if(playerValue[i] == 0) {
 				return true;
@@ -59,20 +62,20 @@ public class BaseballGame {
 	}
 
 	// 스트라이크, 볼 판별
-	public static int[] judgeResult(int i, int[] playerValue, int[] pcValue, int[] result) {
+	public int[] judgeResult(int i, int[] result) {
 		for (int j = 0; j < figures; j++) {
-			if (i == j && playerValue[i] == pcValue[j]) { // 스트라이크의 경우
+			if (i == j && this.playerValue[i] == this.pcValue[j]) { // 스트라이크의 경우
 				result[0]++;
 			}
-			if (i != j && playerValue[i] == pcValue[j]) { // 볼의 경우
+			if (i != j && this.playerValue[i] == this.pcValue[j]) { // 볼의 경우
 				result[1]++;
 			}
-		}
+		} 
 		return result;
 	}
 
 	// 결과값 출력
-	public static void printResult(int strike, int ball) {
+	public void printResult(int strike, int ball) {
 		if (strike != 0) {
 			System.out.printf("%d 스트라이크 ", strike);
 		}
@@ -88,12 +91,11 @@ public class BaseballGame {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int[] pcValue = new int[figures];
-		pcValue = randomValue();
+		BaseballGame game = new BaseballGame();
+		game.pcValue = randomValue();
 
 		while (true) {
 			System.out.print("중복되지 않는 숫자 " + figures + "자리를 입력해주세요 : ");
-			int[] playerValue = new int[figures];
 			String playerInput = scan.next();
 			
 			// 예외처리 : 자리 수보다 적게 입력하거나 많이 입력하였을 경우(피드백 반영)
@@ -104,7 +106,7 @@ public class BaseballGame {
 			
 			// 예외처리 : 정수형이 아닌 값으로 입력받았을 경우(피드백 반영)
 			try {
-				playerValue = valueParse(playerInput);  // 입력값 배열로 정리
+				game.playerValue = valueParse(playerInput);  // 입력값 배열로 정리
 			}
 			catch(NumberFormatException nfe) {
 				System.out.println("[오류]정수형으로 다시 입력해주세요!");
@@ -112,12 +114,12 @@ public class BaseballGame {
 			}
 			
 			// 예외처리 : 중복값 입력했을 경우(피드백 반영)
-			if (duplicateException(playerValue)) {
+			if (game.duplicateException(game.playerValue)) {
 				System.out.println("[오류]수가 중복되지 않도록 다시 입력해주세요!");
 				continue;
 			}
 			// 예외처리 : 0이 입력되었을 경우(피드백 반영)
-			if(zeroException(playerValue)) {
+			if(game.zeroException(game.playerValue)) {
 				System.out.println("[오류]각 자리수에 0이 없도록 다시 입력해주세요!");
 				continue;
 			} 
@@ -125,10 +127,10 @@ public class BaseballGame {
 			int result[] = new int[2]; // result[0] : strike, result[1] : ball
 
 			for (int i = 0; i < figures; i++) {
-				result = judgeResult(i, playerValue, pcValue, result);
+				result = game.judgeResult(i, result);  // 객체화를 통해 judgeResult의 인자수를 줄임(피드백 반영) 
 			}
 
-			printResult(result[0], result[1]);
+			game.printResult(result[0], result[1]); 
 
 			if (result[0] == figures) {
 				System.out.println(figures + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
