da5c52de906049f8e8356f6b10d048333e46ea7b
Brad Lee <brad903@naver.com>
2018-08-03 13:50:35 +0900

    게임 종료 개선
    모두 맞췄을 경우 게임 계속 여부를 물어 전원 종료 판단

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -50,11 +49,11 @@ public class BaseballGame {
 		}
 		return false;
 	}
-	
+
 	// 예외처리 : 입력값에 0이 포함되었을 경우(피드백 반영)
 	public boolean zeroException(int[] playerValue) {
-		for(int i=0; i<figures; i++) {
-			if(playerValue[i] == 0) {
+		for (int i = 0; i < figures; i++) {
+			if (playerValue[i] == 0) {
 				return true;
 			}
 		}
@@ -70,7 +69,7 @@ public class BaseballGame {
 			if (i != j && this.playerValue[i] == this.pcValue[j]) { // 볼의 경우
 				result[1]++;
 			}
-		} 
+		}
 		return result;
 	}
 
@@ -86,7 +85,20 @@ public class BaseballGame {
 			System.out.printf("낫싱");
 		}
 		System.out.println();
+	}
 
+	public boolean judgeOnoff(int result, Scanner scan) {
+		if (result == figures) {
+			System.out.print(figures + "개의 숫자를 모두 맞히셨습니다! 계속하시겠습니까?(y/n) : ");
+			char ans = scan.next().charAt(0);
+			if (ans == 'y') {
+				this.pcValue = randomValue();
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
@@ -97,43 +109,45 @@ public class BaseballGame {
 		while (true) {
 			System.out.print("중복되지 않는 숫자 " + figures + "자리를 입력해주세요 : ");
 			String playerInput = scan.next();
-			
+
 			// 예외처리 : 자리 수보다 적게 입력하거나 많이 입력하였을 경우(피드백 반영)
-			if(playerInput.length() != figures) {
+			if (playerInput.length() != figures) {
 				System.out.println("[오류]" + playerInput.length() + "자리의 수가 아닌 " + figures + "자리의 수를 입력해주세요!");
 				continue;
 			}
-			
+
 			// 예외처리 : 정수형이 아닌 값으로 입력받았을 경우(피드백 반영)
 			try {
-				game.playerValue = valueParse(playerInput);  // 입력값 배열로 정리
-			}
-			catch(NumberFormatException nfe) {
+				game.playerValue = valueParse(playerInput); // 입력값 배열로 정리
+			} catch (NumberFormatException nfe) {
 				System.out.println("[오류]정수형으로 다시 입력해주세요!");
 				continue;
 			}
-			
+
 			// 예외처리 : 중복값 입력했을 경우(피드백 반영)
 			if (game.duplicateException(game.playerValue)) {
 				System.out.println("[오류]수가 중복되지 않도록 다시 입력해주세요!");
 				continue;
 			}
 			// 예외처리 : 0이 입력되었을 경우(피드백 반영)
-			if(game.zeroException(game.playerValue)) {
+			if (game.zeroException(game.playerValue)) {
 				System.out.println("[오류]각 자리수에 0이 없도록 다시 입력해주세요!");
 				continue;
-			} 
+			}
 
 			int result[] = new int[2]; // result[0] : strike, result[1] : ball
 
 			for (int i = 0; i < figures; i++) {
-				result = game.judgeResult(i, result);  // 객체화를 통해 judgeResult의 인자수를 줄임(피드백 반영) 
+				result = game.judgeResult(i, result); // 객체화를 통해 judgeResult의 인자수를 줄임(피드백 반영)
 			}
 
-			game.printResult(result[0], result[1]); 
+			game.printResult(result[0], result[1]);
 
-			if (result[0] == figures) {
-				System.out.println(figures + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			boolean ans = game.judgeOnoff(result[0], scan);
+			if (ans) {
+				continue;
+			} else {
+				System.out.println("게임 종료");
 				scan.close();
 				break;
 			}
